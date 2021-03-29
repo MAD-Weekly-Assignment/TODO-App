48cf363ada97cfcb153eecc8ed9e8bc24feed1ba
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-29 12:06:27 +0545

    Listing tasks in repository

--- TODOApp/app/src/main/java/com/example/todo_app/data/Repository.java
+++ TODOApp/app/src/main/java/com/example/todo_app/data/Repository.java
@@ -1,7 +1,28 @@
 package com.example.todo_app.data;
 
+import android.app.Application;
+
+import java.util.List;
+
 public class Repository {
 
+    private static Repository INSTANCE;
+    AppDatabase db;
+    TodoDao dao;
 
+    private Repository(Application application) {
+        db = AppDatabase.getDatabase(application);
+        dao = db.todoDao();
+    }
 
+    public static Repository getRepository(Application application) {
+        if (INSTANCE == null) {
+            INSTANCE = new Repository(application);
+        }
+        return INSTANCE;
+    }
+
+    public List<Task> getAllTasks() {
+        return dao.getAllTasks();
+    }
 }
