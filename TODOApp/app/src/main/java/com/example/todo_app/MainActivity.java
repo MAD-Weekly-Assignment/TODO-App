b82cd217def4b8c37d9d3dfa0c4aaf8000e1c739
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-29 12:52:30 +0545

    Created a Intent onClick to switch to AddTaskActivity from MainActivity

--- TODOApp/app/src/main/java/com/example/todo_app/MainActivity.java
+++ TODOApp/app/src/main/java/com/example/todo_app/MainActivity.java
@@ -1,35 +1,54 @@
 package com.example.todo_app;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.todo_app.data.AppDatabase;
+import com.example.todo_app.data.Repository;
 import com.example.todo_app.data.Task;
 import com.example.todo_app.data.TodoDao;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
-
 public class MainActivity extends AppCompatActivity {
+
     public static String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView recyclerView;
+
+    private Repository repository;
+    private List<Task> taskList;
+    private TaskAdapter adapter;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppDatabase db = AppDatabase.getDatabase(this);
-        TodoDao dao = db.todoDao();
-        AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+        recyclerView = findViewById(R.id.task_list);
+        fab = findViewById(R.id.floatingActionButton);
+
+        repository = Repository.getRepository(this.getApplication());
+        taskList = repository.getAllTasks();
+        adapter = new TaskAdapter();
+        adapter.setDate(taskList);
+        recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void run() {
-                List<Task> tasks = dao.getAllTasks();
-                Log.d(TAG, ""+tasks.size());
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+
             }
         });
-
-
     }
 }
