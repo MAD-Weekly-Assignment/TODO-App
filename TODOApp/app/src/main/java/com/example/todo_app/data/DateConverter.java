16858e0dd81f48cb68d488e3c6b623a76bf7b3d4
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-29 11:51:55 +0545

    DateConverter created in the data

--- TODOApp/app/src/main/java/com/example/todo_app/data/DateConverter.java
+++ TODOApp/app/src/main/java/com/example/todo_app/data/DateConverter.java
@@ -1,5 +1,18 @@
 package com.example.todo_app.data;
 
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
 public class DateConverter {
 
+    @TypeConverter
+    public static Date toDate(Long timeStamp){
+        return timeStamp == null ? null : new Date(timeStamp);
+    }
+
+    @TypeConverter
+    public static Long toTimeStamp(Date date){
+        return date == null ? null : date.getTime();
+    }
 }
